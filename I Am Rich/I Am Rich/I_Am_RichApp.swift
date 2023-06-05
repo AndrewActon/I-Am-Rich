402f3d82668bf6edc1d6910407fda30a2e8767de
Andrew Acton <andrew.acton.aa@gmail.com>
2023-06-05 14:45:21 -0600

    Simple SwiftUI

---
+++ I Am Rich/I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Andrew Acton on 6/5/23.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
